97f1888f34e8114b290ca994250951a79a4064eb
Ermi <manermidem@gmail.com>
2023-02-22 15:41:12 +0300

    Remove unwanted line for clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Math from './pages/calculatorPage';
 import Quote from './pages/quotePage';
 import Home from './pages/homePage';
 import NavBar from './components/navBar/navBar';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/calculator" element={<Math />} />
-            <Route path="/quote" element={<Quote />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Math />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
