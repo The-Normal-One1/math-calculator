46a6413a0c7fa7516deead23456c219171e44710
Ermi <manermidem@gmail.com>
2023-02-22 13:38:56 +0300

    Create three pages

---
+++ src/pages/homePage.js
@@ -0,0 +1,26 @@
+// /* eslint-disable max-len */
+import React from 'react';
+import '../components/Calculator.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2 className="homeHeader">Welcome to our page!</h2>
+      <p className="homePara">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam
+        cumque similique quidem nemo alias iusto officia, unde doloribus iste
+        suscipit vel omnis quis fugiat provident quibusdam nobis rem dolor
+        assumenda aut? Sed ipsa nam molestiae molestias saepe inventore! Velit!
+      </p>
+
+      <p className="homePara">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam
+        cumque similique quidem nemo alias iusto officia, unde doloribus iste
+        suscipit vel omnis quis fugiat provident quibusdam nobis rem dolor
+        assumenda aut? Sed ipsa nam molestiae molestias saepe inventore! Velit!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
